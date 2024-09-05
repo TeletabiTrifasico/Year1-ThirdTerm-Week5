1a0c12d18dd79c05ab8bd353da8b48c8ddbaa232
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-09-05 17:45:50 +0200

    Add files via upload

---
+++ assignment1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Model;
+using ReservationSystemService;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+
+        void Start()
+        {
+            CustomerService customerService = new CustomerService();
+            BookService bookService = new BookService();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("testing CustomerSerivce");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.Write("Enter customer id: ");
+            int customerId = int.Parse(Console.ReadLine());
+            Console.WriteLine(customerService.GetById(customerId));
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("testing BookSerivce");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.Write("Enter name of author: ");
+            string authorName = Console.ReadLine();
+            List<Book> books = bookService.GetByAuthor(authorName);
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
+
